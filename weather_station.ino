8530dc21171d02a04c8c70bdc76a82636efbb872
Hunter Allen <allenhm@gmail.com>
2016-05-07 23:28:06 -0400

    Added date/time input. Provides timestamp for log.

--- weather_station.ino
+++ weather_station.ino
@@ -11,6 +11,7 @@
 
 #include <DHT.h>
 #include <SdFat.h>
+#include <TimeLib.h>
 #include <Wire.h>
 #include "IntersemaBaro_MODIFIED.h"
 
@@ -25,6 +26,7 @@ DHT dht(DHT11Pin, DHT11);
 
 Intersema::BaroPressure_MS5607B baro(true);
 
+const bool requestDateTime = true;
 const bool dataLogging = true;
 
 const int dataIntervalSec = 5; // Interval in seconds between successive data read/write events
@@ -55,6 +57,8 @@ void setup() {
     unsigned long pressPa = baro.getPressurePascals();
     delay(100);
   }
+
+  if (requestDateTime == true) inputDateTime();
 }
 
 void loop() {
@@ -67,9 +71,24 @@ void loop() {
   unsigned long pressPa = baro.getPressurePascals();
   delay(100);
 
-  //String dataString = String(lightVal) + ", " + String(tempF) + ", " + String(humidity) + ", " + String(pressPa);
-  String dataString = String(tempF) + ", " + String(humidity) + ", " + String(pressPa);
-  Serial.println(dataString);
+  String dataString;
+
+  if (requestDateTime == true) {
+    String minuteFormatted = formatDigits(minute());
+    String secondFormatted = formatDigits(second());
+
+    String dateString = String(month()) + "/" + String(day()) + "/" + String(year());
+    String timeString = String(hour()) + ":" + minuteFormatted + ":" + secondFormatted;
+
+    //String dataString = dateString + ", " + timeString + ", " + String(lightVal) + ", " + String(tempF) + ", " + String(humidity) + ", " + String(pressPa);
+    dataString = dateString + ", " + timeString + ", " + String(tempF) + ", " + String(humidity) + ", " + String(pressPa);
+  }
+  else {
+    unsigned long elapsedTime = millis();
+    //String dataString = String(elapsedTime) + ", " + String(lightVal) + ", " + String(tempF) + ", " + String(humidity) + ", " + String(pressPa);
+    dataString = String(elapsedTime) + ", " + String(tempF) + ", " + String(humidity) + ", " + String(pressPa);
+    Serial.println(dataString);
+  }
 
   if (dataLogging == true) {
     Serial.print("Writing to SD card...");
@@ -83,8 +102,123 @@ void loop() {
 void writeSD(String dataString) {
   if (!logFile.open("data_log.txt", O_RDWR | O_CREAT | O_AT_END)) sd.errorHalt("Failed to open log file.");
 
-  //Serial.print("writing data to log file...");
   logFile.println(dataString);
-  //Serial.println("complete.");
   logFile.close();
+}
+
+void inputDateTime() {
+  String monthInput, dayInput, yearInput, hourInput, minuteInput, secondInput;
+
+  Serial.println("Please input date.");
+  Serial.println();
+  Serial.print("Month: ");
+
+  while (!Serial.available()) {
+    delay(10);
+  }
+
+  while (Serial.available()) {
+    char c = Serial.read();
+    if (c != '\n' || c != '\r') monthInput += c;
+    delay(10);
+  }
+  int monthInt = monthInput.toInt();
+
+  Serial.println(monthInt);
+  Serial.print("Day: ");
+
+  while (!Serial.available()) {
+    delay(10);
+  }
+
+  while (Serial.available()) {
+    char c = Serial.read();
+    if (c != '\n' || c != '\r') dayInput += c;
+    delay(10);
+  }
+  int dayInt = dayInput.toInt();
+
+  Serial.println(dayInt);
+  Serial.print("Year: ");
+
+  while (!Serial.available()) {
+    delay(10);
+  }
+
+  while (Serial.available()) {
+    char c = Serial.read();
+    if (c != '\n' || c != '\r') yearInput += c;
+    delay(10);
+  }
+  int yearInt = yearInput.toInt();
+
+  /*Serial.println(yearInt);
+  Serial.println();
+  String dateInput = String(monthInt) + "/" + String(dayInt) + "/" + String(yearInt);
+  Serial.println(dateInput);
+  Serial.println();
+  Serial.println("Please input time in 24-hour format.");
+  Serial.println();
+  Serial.print("Hour: ");*/
+
+  while (!Serial.available()) {
+    delay(10);
+  }
+
+  while (Serial.available()) {
+    char c = Serial.read();
+    if (c != '\n' || c != '\r') hourInput += c;
+    delay(10);
+  }
+  int hourInt = hourInput.toInt();
+
+  Serial.println(hourInt);
+  Serial.print("Minute: ");
+
+  while (!Serial.available()) {
+    delay(10);
+  }
+
+  while (Serial.available()) {
+    char c = Serial.read();
+    if (c != '\n' || c != '\r') minuteInput += c;
+    delay(10);
+  }
+  int minuteInt = minuteInput.toInt();
+
+  Serial.println(minuteInt);
+  Serial.print("Second: ");
+
+  while (!Serial.available()) {
+    delay(10);
+  }
+
+  while (Serial.available()) {
+    char c = Serial.read();
+    if (c != '\n' || c != '\r') secondInput += c;
+    delay(10);
+  }
+  int secondInt = secondInput.toInt();
+
+  /*Serial.println(secondInt);
+  Serial.println();
+  String minuteFormatted = formatDigits(minuteInt);
+  String secondFormatted = formatDigits(secondInt);
+  String timeInput = String(hourInt) + ":" + minuteFormatted + ":" + secondFormatted;
+  Serial.println(timeInput);
+  Serial.println();*/
+
+  setTime(hourInt, minuteInt, secondInt, dayInt, monthInt, yearInt);
+
+  //time_t timeNow = now();
+  //Serial.println(timeNow);
+}
+
+String formatDigits(int digits) {
+  String digitsFormatted;
+
+  if (digits < 10) digitsFormatted = '0' + String(digits);
+  else digitsFormatted = String(digits);
+
+  return digitsFormatted;
 }
